1d2c448052b7a46e9eba82db650b0506ca77c685
Lava Ghimire <ghimirelava2@gmail.com>
2026-04-09 19:34:36 +0000

    feat: add configuration for GPU sequential processing

---
+++ serving/configs/gpu_sequential.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    "mode": "gpu_sequential",
+    "device": "gpu",
+    "model_type": "pytorch",
+    "num_users": 100,
+    "uploads_per_user": 20,
+    "max_workers": 1, 
+    "url": "http://127.0.0.1:8000/predict",
+}
